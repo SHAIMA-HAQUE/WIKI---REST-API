17646418976b1c1215bc71a3689cb47f9e3fa7b6
Shaima Haque <shaimahaque02@yahoo.com>
2021-09-04 10:22:17 +0530

    Initial commit

---
+++ document.md
@@ -0,0 +1,26 @@
+{
+        "_id": "6086dfdcea8ead397fc52507",
+        "title": "REST",
+        "content": "It is short for REpresentational State Transfer"
+    },
+    {
+        "_id": "6086e387ea8ead397fc5257b",
+        "title": "API",
+        "content": "It is Application Programming Language"
+    },
+    {
+        "_id": "6086e423ea8ead397fc52599",
+        "title": "CSS",
+        "content": "It stands for Cascading Styles Sheet"
+    },
+    {
+        "_id": "613232108489abb519966f40",
+        "title": "List in Python",
+        "content": "A list is a way to keep track of an ordered collection of    items."
+    },
+    {
+        "_id": "61323b6098a3cd6378953004",
+        "title": "Denny Crane",
+        "content": "Denny Crane has the mad cow disease",
+        "__v": 0
+    }
---
+++ index.js
@@ -0,0 +1,68 @@
+
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const express = require("express");
+
+const app = express();
+
+app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB",{useNewUrlParser:true});
+
+app.route("/articles")
+.get(function(req,res){
+  Article.find(function(err,foundArticles){
+    if(!err){
+      res.send(foundArticles);
+    }else{
+      res.send(err);
+    }
+    // console.log(foundArticles);
+    
+  })
+})
+.post(function(req,res){
+  console.log(req.body.title);
+  console.log(req.body.content);
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  })
+  newArticle.save(function(err){
+    if(!err){
+      res.send("Successfully added a new article.");
+    }else{
+      res.send(err);
+    }
+  });
+})
+.delete(function(req,res){
+  Article.deleteMany(function(err){
+    if(!err){
+      res.send("Successfully deleted all articles.");
+    }else{
+      res.send(err);
+    }
+  })
+});
+
+const articleSchema = {
+  title:String,
+  content: String
+}
+
+const Article = mongoose.model("Article",articleSchema);
+
+
+
+app.listen(3000,function(req,res){
+  console.log("Server is up and running");
+})
